caf477de07492ba1a0e587ab910d96fee7e985df
Nazar <nazarfr43@gmail.com>
2024-12-26 18:00:43 +0000

    Configurada API de movieCatalog y creado su restTemplate

--- movie-catalog-service/src/main/java/com/nazar/movie_catalog_service/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/nazar/movie_catalog_service/controller/MovieCatalogController.java
@@ -1,21 +1,42 @@
 package com.nazar.movie_catalog_service.controller;
 
 import com.nazar.movie_catalog_service.model.CatalogItem;
+import com.nazar.movie_catalog_service.model.Movie;
+import com.nazar.movie_catalog_service.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public MovieCatalogController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return List.of(
-                new CatalogItem("Transformers", "Test", 4),
-                new CatalogItem("Avengers", "Test", 5)
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
         );
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+        }).collect(Collectors.toList());
     }
 
 }
